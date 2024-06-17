a6df00c444dee29dbce940908d842ae97c75b260
EmanuelMarkov <164407831+EmanuelMarkov@users.noreply.github.com>
2024-06-17 08:42:57 +0300

    Update What_is_your_age.py

--- My beginner projects in python/What_is_your_age/What_is_your_age.py
+++ My beginner projects in python/What_is_your_age/What_is_your_age.py
@@ -8,5 +8,5 @@ print("Hi " + name + " you will turn 100 in the year " +  str(turn_100))
 
 annoy = int(input("So how much times do you want to hear that "))
 while annoy>0:
-    print("Hi " + name + " you will turn 100 in the year " +  str(turn_100) + "\n")
+    print("Hiiii " + name + " you will turn 100 in the year " +  str(turn_100) + "\n")
     annoy -= 1
