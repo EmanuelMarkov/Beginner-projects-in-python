63a217e213f9906c87cb01d6397e6c14a222f94c
EmanuelMarkov <emomarkov580@gmail.com>
2024-06-17 16:40:57 +0300

    Lists

---
+++ Lists.py
@@ -0,0 +1,16 @@
+food = ["pizza","tacos","ice cream","pudding"]
+food[0]="sushi"
+print(food[3])
+print()
+for x in food:
+    print(x)
+
+food.append("bao buns")
+food.remove("pudding")
+food.insert("cake")
+food.sort()
+
+for x in food:
+    print(x)
+
+food.clear()
