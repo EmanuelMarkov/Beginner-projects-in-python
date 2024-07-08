3476bb5804ae59af7cc91d5cd91a507f5e6cdbf0
EmanuelMarkov <emomarkov580@gmail.com>
2024-07-08 23:01:05 +0300

    More work

---
+++ List overlap.py
@@ -0,0 +1,12 @@
+import random
+a = []
+b = []
+i = random(1,100)
+while random(1,10)>0:
+    a.append
+
+
+
+
+common_elements = list(set(a).intersection(set(b)))
+print(common_elements)
---
+++ Playing around.py
@@ -0,0 +1,3 @@
+cats = {"black","orange","white"}
+cats1 = {"black","white"}
+print(len(cats1))
